8e2aef3fa04bfe7579a88061d98bf7aded509131
Emilia Augustyn <emiliaugustyn@gmail.com>
2020-10-25 12:51:09 +0100

    logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        :src="require('../assets/w4_med.png')"
+        width="100"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NavBar",
+});
+</script>
